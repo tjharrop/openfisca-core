3936044ab900f713348b31811398db55b99ac826
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-08-08 18:15:01 +0200

    It works, but what about perf ?

--- test.py
+++ test.py
@@ -56,7 +56,7 @@ x = array(
     [(x_zone_1, x_zone_2, x_coloc)],
     dtype=[('zone_1', x_zone_1.dtype), ('zone_2', x_zone_2.dtype), ('coloc', x_coloc.dtype)])
 
-# x = x.view(numpy.recarray)
+x = x.view(numpy.recarray)
 
 # Tentative d'extension de la classe
 
@@ -70,21 +70,29 @@ class CustomArray(object):
 
     def __getitem__(self, key):
         if isinstance(key, numpy.ndarray):
-            # Get the parameters values indexed by names, without metadata
-            names = [name for name in self.vector.dtype.names]
-            values = [self.vector[k] for k in names]
-            import numpy_indexed as npi
-            remapped_array = npi.remap(key, names, values)
-            from pprint import pprint
-            import ipdb
-            ipdb.set_trace()
+            dtype = self.vector[key[0]].dtype
+            return array([self.vector[k][0] for k in key], dtype=dtype).view(numpy.recarray)
         else:
             return self.vector[key]
 
 y = CustomArray(x)
 
 
-vector = numpy.asarray(['zone_1', 'zone_1'])
+vector = numpy.asarray(['zone_1', 'zone_2', 'zone_1', 'zone_1'])
 z = y[vector]
 
 
+from pprint import pprint
+import ipdb
+ipdb.set_trace()
+
+# Conclusion :
+#  - rec arrays are interesting, as they are numpy objects requestables with the "." notation
+#  - It is not possible not use fancy indexing on the field of a rec array
+
+
+# Quel est l'output possible du fancy indexing ?
+# y[[z1, z2]] = array([(2,4), (2,4)], dtype=(('couple', float), ('seul', float)))
+# Est-ce qu'on peut avoir un niveau de nesting de plus ?
+# Oui, au moins en théorie
+# On y arrive en bouclant, mais perf ?
