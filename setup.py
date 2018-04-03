6dbe2bb3c4a1a7fbee8e3cb1b2b9810e1d708ed0
Florian Pagnoux <florian.pagnoux@gmail.com>
2018-04-03 13:44:26 -0400

    WIP

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'OpenFisca-Core',
-    version = '22.0.3.dev0',
+    version = '22.0.3.dev1',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [
