32db42775678e5e7f27b6a362c9e3759859b8c84
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-11-17 20:32:33 +0100

    WIP

--- openfisca_core/scripts/measure_performances_dump.py
+++ openfisca_core/scripts/measure_performances_dump.py
@@ -7,13 +7,16 @@
 
 from __future__ import unicode_literals, print_function, division, absolute_import
 import argparse
+import tempfile
 import logging
+import shutil
 import sys
 import time
 
 
 from openfisca_core.scripts.simulation_generator import make_simulation, randomly_init_variable
 from openfisca_france import CountryTaxBenefitSystem
+from openfisca_core.tools.simulation_dumper import dump_simulation, restore_simulation
 
 args = None
 
@@ -29,12 +32,16 @@ def timeit(method):
     return timed
 
 
-@timeit
 def test(nb_persons = 400, nb_groups = 100):
-    tbs = CountryTaxBenefitSystem()
-    simulation = make_simulation(tbs, nb_persons, nb_groups)  # Create a simulation with 400 persons, spread among 100 families
+    tax_benefit_system = CountryTaxBenefitSystem()
+    simulation = make_simulation(tax_benefit_system, nb_persons, nb_groups)  # Create a simulation with 400 persons, spread among 100 families
     randomly_init_variable(simulation, 'salaire_de_base', 2017, max_value = 50000, condition = simulation.persons.has_role(simulation.famille.DEMANDEUR))  # Randomly set a salaire_net for all persons between 0 and 50000?
-    print(simulation.calculate('revenu_disponible', 2017))
+    simulation.calculate('revenu_disponible', 2017)
+    directory = tempfile.mkdtemp()
+    timeit(dump_simulation)(simulation, directory)
+    del simulation
+    timeit(restore_simulation)(directory, tax_benefit_system)
+    shutil.rmtree(directory)
 
 
 def main():
@@ -44,7 +51,7 @@ def main():
     args = parser.parse_args()
     logging.basicConfig(level = logging.DEBUG if args.verbose else logging.WARNING, stream = sys.stdout)
 
-    for nb_persons in [1e4, 1e5, 1e6, 1e7]:
+    for nb_persons in [1e3, 1e4]:
         nb_persons = int(nb_persons)
         nb_groups = int(nb_persons / 4)
         test(nb_persons, nb_groups)
