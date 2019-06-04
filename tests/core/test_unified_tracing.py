f7d651db8f9fc2efc6c0b40a57f2b490a53aec4a
sandcha <sandcha@users.noreply.github.com>
2019-06-04 11:33:01 +0200

    Init full tracing

--- tests/core/test_unified_tracing.py
+++ tests/core/test_unified_tracing.py
@@ -1,49 +1,52 @@
 # -*- coding: utf-8 -*-
 
-from openfisca_core.unified_tracing import SimpleTracer
+from openfisca_core.unified_tracing import SimpleTracer, FullTracer
 from pytest import fixture
 
 
 class DummySimulation:
 
-    def __init__(self):
-        self.tracer = SimpleTracer()
+    def __init__(self, tracer):
+        self.tracer = tracer
 
     @property
     def stack(self):
         return self.tracer.stack
 
     def calculate(self, variable, period):
-        # with self.tracer.record(variable.__class__.__name__, period) as frame:
-        self.tracer.record(variable.__class__.__name__, period)    
-        
-        variable.formula(self, period)
+        print("> calculate ", variable.__class__.__name__)
+
+        self.tracer.record(variable.__class__.__name__, period)     
+        variable.formula(period)
+        print("end formula", variable.__class__.__name__)
+        self.tracer.pop()
+
 
 class v0:
 
-    def __init__(self, simulation):
-        self.simulation = DummySimulation()
+    def __init__(self, tracer):
+        self.simulation = DummySimulation(tracer)
 
-    def formula(self, simulation, period):
-        simulation.calculate(v1(), period) # v0 v1
-        simulation.calculate(v2(), period) # v0 v2
+    def formula(self, period):
+        self.simulation.calculate(v1(), period) # v0 v1
+        self.simulation.calculate(v2(), period) # v0 v2
 
 
 class v1:
 
-    def formula(self, simulation, period):
+    def formula(self, period):
         pass
 
 
 class v2:
 
-    def formula(self, simulation, period):
+    def formula(self, period):
         pass
 
 
 @fixture
 def simulation():
-    return DummySimulation()
+    return DummySimulation(SimpleTracer())
 
 
 def test_stack_one_level():
@@ -54,13 +57,13 @@ def test_stack_one_level():
     assert frame.stack == {}
 
 
-def test_record(simulation):
-    variable = v0(simulation)
+def test_record():
+    variable = v0(FullTracer())
     period = '2019-01'
 
-    simulation.calculate(variable, period)
+    variable.simulation.calculate(variable, period)
 
-    assert simulation.stack == {
+    assert variable.simulation.stack == {
         'name': 'v0',
         'period': '2019-01',  
         'children': [
@@ -74,3 +77,12 @@ def test_record(simulation):
             }
         ]
     }
+
+
+def test_pop():
+    variable = v0(SimpleTracer())
+    period = '2019-01'
+
+    variable.simulation.calculate(variable, period)
+    
+    assert variable.simulation.stack == {}
