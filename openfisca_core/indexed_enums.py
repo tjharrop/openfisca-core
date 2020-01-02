de142e08ff07b1c95a49e1956e8760127ac39fb6
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2020-01-02 05:15:37 +0100

    Improve indexed_enums.py documentation

--- openfisca_core/indexed_enums.py
+++ openfisca_core/indexed_enums.py
@@ -85,7 +85,7 @@ class Enum(BaseEnum):
             # gives them a different identity from the ones imported in the usual way.
             # So, instead of relying on the "cls" passed in, we use only its name to
             # check that the values in the array, if non-empty, are of the right type.
-            array_size: bool = size(array)
+            array_size: int = size(array)
             array_name: str = take(array, 0).__class__.__name__
 
             if array_size > 0 and array_name is cls.__name__:
